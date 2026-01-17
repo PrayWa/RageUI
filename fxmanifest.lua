df33067725d43ec0e67399d1eb2b1625e830b1c2
UnderTheMap <251097252+UnderTheMap@users.noreply.github.com>
2026-01-17 04:04:49 +0100

    Add RageUI V3 menu system and components
    Initial commit of RageUI V3, including core menu logic, UI components, panels, items, and supporting assets for GTA V. This provides a modular, extensible menu system for FiveM, with support for custom UI elements, audio, and visual feedback.

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+version '3.0.0' -- RageUI V1 and V2 are not like this one.
+
+author 'Frazzle, Dylan Malandain, Kalyptus, Parow, TDLC, eR0ot, ImBaphomettt (Sensity)'
+description 'Reuploaded "V3" RageUI versions.'
+
+client_scripts {
+    "src/RMenu.lua",
+    "src/menu/RageUI.lua",
+    "src/menu/Menu.lua",
+    "src/menu/MenuController.lua",
+    "src/components/*.lua",
+    "src/menu/elements/*.lua",
+    "src/menu/items/*.lua",
+    "src/menu/panels/*.lua",
+    "src/menu/panels/*.lua",
+    "src/menu/windows/*.lua",
+
+    "example.lua"
+}
+
+files {
+    'stream/*.ytd'
+}
